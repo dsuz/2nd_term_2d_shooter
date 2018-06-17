51f06ebc8f742389ca9f0f6d95a2b917363602fe
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-17 23:29:42 +0900

    プレイヤーを上下左右に操作する

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_moveSpeed = 5f;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");   // 垂直方向の入力を取得する
+        float v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
+        Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction)
+        m_rb2d.velocity = dir * m_moveSpeed;        // 単位ベクトルにスピードをかけて速度ベクトルにする
+    }
+}
