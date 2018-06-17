1d942f977a8a2d61032ca432a92624fb4c80ab85
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-17 23:46:44 +0900

    弾が飛ぶところまで

---
+++ Assets/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    /// <summary>弾の飛ぶ速度</summary>
+    [SerializeField] float m_bulletSpeed = 10f;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+        Vector3 dir = Vector2.up * m_bulletSpeed;   // 弾が飛ぶ速度ベクトルを計算する
+        m_rb2d.velocity = dir;                      // 速度ベクトルを弾にセットする
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>プレイヤーの移動速度</summary>
     [SerializeField] float m_moveSpeed = 5f;
     Rigidbody2D m_rb2d;
 
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");   // 垂直方向の入力を取得する
         float v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
-        Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction)
+        Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction) 
         m_rb2d.velocity = dir * m_moveSpeed;        // 単位ベクトルにスピードをかけて速度ベクトルにする
     }
 }
