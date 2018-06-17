4d394fbb7d12eb2944f4138ca286c50ad5ebdd93
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-17 23:56:36 +0900

    スペースまたは左クリックで弾を発射する

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     /// <summary>プレイヤーの移動速度</summary>
     [SerializeField] float m_moveSpeed = 5f;
     Rigidbody2D m_rb2d;
+    /// <summary>弾のプレハブ</summary>
+    [SerializeField] GameObject m_bulletPrefab;
 
     void Start()
     {
@@ -19,5 +21,11 @@ public class PlayerController : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
         Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction) 
         m_rb2d.velocity = dir * m_moveSpeed;        // 単位ベクトルにスピードをかけて速度ベクトルにする
+
+        // 左クリックまたはスペースで弾を発射する
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(m_bulletPrefab, this.transform.position, Quaternion.identity);  // インスペクターから設定した m_bulletPrefab をインスタンス化する
+        }
     }
 }
