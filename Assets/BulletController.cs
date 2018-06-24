b6136eba89aa58cd1153d82d33ad7a6083e3e8df
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-25 01:37:55 +0900

    敵の破壊音を加えた

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -9,12 +9,14 @@ public class BulletController : MonoBehaviour
     Rigidbody2D m_rb2d;
     /// <summary>ここまで飛んだら弾を消す</summary>
     [SerializeField] float m_bulletTravelDistance = 20f;
+    AudioSource m_audioSource;
 
     void Start()
     {
         m_rb2d = GetComponent<Rigidbody2D>();
         Vector3 dir = Vector2.up * m_bulletSpeed;   // 弾が飛ぶ速度ベクトルを計算する
         m_rb2d.velocity = dir;                      // 速度ベクトルを弾にセットする
+        m_audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -23,6 +25,14 @@ public class BulletController : MonoBehaviour
         if (this.transform.position.y > m_bulletTravelDistance)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            m_audioSource.Play();
         }
     }
 }
