3be5f6ea6ad16fffd07212a2b932ce65a434babe
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-24 21:53:32 +0900

    敵が弾を出すようにした

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] GameObject m_bulletPrefab;
+    [SerializeField] float m_shotInterval = 1.5f;
+    float m_timer;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        
+        if (m_timer > m_shotInterval)
+        {
+            Instantiate(m_bulletPrefab, this.transform.position, Quaternion.identity);
+            m_timer = 0;
+        }
+    }
+}
