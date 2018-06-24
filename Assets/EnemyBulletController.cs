1f5acd9dd2c122b14211a5b4f1835ff714d2086d
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-24 21:41:33 +0900

    コメントを追加した

--- Assets/EnemyBulletController.cs
+++ Assets/EnemyBulletController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>敵の弾を制御するクラス</summary>
 public class EnemyBulletController : MonoBehaviour
 {
+    /// <summary>弾が飛ぶ速度</summary>
     [SerializeField] float m_speed = 0.5f;
     Rigidbody2D m_rb2d;
 
@@ -11,9 +13,8 @@ public class EnemyBulletController : MonoBehaviour
     {
         m_rb2d = GetComponent<Rigidbody2D>();
 
-        // プレイヤーの位置を取得する
         GameObject m_player = GameObject.FindGameObjectWithTag("Player");
-        Vector2 direction = (m_player.transform.position - this.transform.position).normalized;
-        m_rb2d.AddForce(direction, ForceMode2D.Impulse);
+        Vector2 direction = (m_player.transform.position - this.transform.position).normalized;     // プレイヤーのいる方向の単位ベクトルを計算する
+        m_rb2d.AddForce(direction, ForceMode2D.Impulse);    // プレイヤーに向かって弾を飛ばす
     }
 }
