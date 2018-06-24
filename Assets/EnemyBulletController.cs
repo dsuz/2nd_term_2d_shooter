1fa7e7435f560f5b0f16018760fcb090f48220a6
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-06-25 01:16:59 +0900

    敵の弾が消えるようにした

--- Assets/EnemyBulletController.cs
+++ Assets/EnemyBulletController.cs
@@ -7,6 +7,8 @@ public class EnemyBulletController : MonoBehaviour
 {
     /// <summary>弾が飛ぶ速度</summary>
     [SerializeField] float m_speed = 0.5f;
+    /// <summary>弾が飛ぶ範囲</summary>
+    [SerializeField] float m_border = 10f;
     Rigidbody2D m_rb2d;
 
     void Start()
@@ -16,5 +18,15 @@ public class EnemyBulletController : MonoBehaviour
         GameObject m_player = GameObject.FindGameObjectWithTag("Player");
         Vector2 direction = (m_player.transform.position - this.transform.position).normalized;     // プレイヤーのいる方向の単位ベクトルを計算する
         m_rb2d.AddForce(direction, ForceMode2D.Impulse);    // プレイヤーに向かって弾を飛ばす
+    }
+
+    void Update()
+    {
+        // 縦方向または横方向に -10 ~ 10 の範囲から飛び出したら弾を消す
+        if (Mathf.Abs(this.transform.position.x) > m_border
+            || Mathf.Abs(this.transform.position.y) > m_border)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
