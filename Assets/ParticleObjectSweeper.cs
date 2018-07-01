1c0114481fc53961502d9ad2dd1be03a746b02f9
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-07-02 01:04:29 +0900

    爆発の Particle オブジェクトが再生後に消えるようにした

---
+++ Assets/ParticleObjectSweeper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Particle System が追加されたオブジェクトに追加することで、再生が終了したらオブジェクトを破棄するようにする
+/// </summary>
+public class ParticleObjectSweeper : MonoBehaviour
+{
+    /// <summary>Particle System の再生時間</summary>
+    float m_duration;
+    /// <summary>タイマー</summary>
+    float m_timer;
+
+    void Start()
+    {
+        // Particle System の再生時間を取得する
+        m_duration = GetComponent<ParticleSystem>().main.duration;
+    }
+
+    void Update()
+    {
+        // タイマーで時間を測り、再生時間を超えたらオブジェクトを消す
+        m_timer += Time.deltaTime;
+        if (m_timer > m_duration)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
