5426f67d6165c0ad0f2abd54a5ec7a946e386e77
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-07-09 02:16:25 +0900

    ボスがやられた時に爆発の演出を加えた

--- Assets/BossController.cs
+++ Assets/BossController.cs
@@ -6,6 +6,10 @@ public class BossController : MonoBehaviour
 {
     /// <summary>ボスのライフ</summary>
     [SerializeField] int m_life = 10;
+    /// <summary>ダメージを受けた時の爆発エフェクト</summary>
+    [SerializeField] GameObject m_smallExplosion;
+    /// <summary>やられた時の爆発エフェクト</summary>
+    [SerializeField] GameObject m_bigExplosion;
     /// <summary>ボスの生死フラグ</summary>
     bool m_isDead = false;
 
@@ -29,6 +33,8 @@ public class BossController : MonoBehaviour
         {
             m_life -= 1;
             Debug.Log(gameObject.name + "'s life: " + m_life.ToString());
+            // 爆発エフェクトを再生する
+            Instantiate(m_smallExplosion, collision.gameObject.transform.position + new Vector3(0, 0, -5f), Quaternion.identity);   // position はカメラに寄せる
         }
 
         // ライフが 0 になったらやられたことにする
@@ -36,6 +42,15 @@ public class BossController : MonoBehaviour
         {
             m_isDead = true;
             Debug.Log(gameObject.name + " is Dead.");
+            // 爆発エフェクトを生成する
+            GameObject go = Instantiate(m_bigExplosion, this.transform.position + new Vector3(0, 0, -5f), Quaternion.identity);   // position はカメラに寄せる
+            // 爆発エフェクトのループをオンにする
+            ParticleSystem[] psArray = go.transform.GetComponentsInChildren<ParticleSystem>();
+            foreach(var ps in psArray)
+            {
+                var psMain = ps.main;
+                psMain.loop = true;
+            }
         }
     }
 }
