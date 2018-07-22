c2ffe307325ceb7dfeae723e08d12b02e2c79cdb
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-07-22 23:39:34 +0900

    スクロールを作った

---
+++ Assets/ScrollController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollController : MonoBehaviour
+{
+    [SerializeField] float m_scrollSpeed = 1.0f;
+
+    /// <summary>
+    /// スクロール速度。正の値の時には下へスクロールする。負の値の時には上へスクロールする
+    /// </summary>
+    public float ScrollSpeed
+    {
+        get { return this.m_scrollSpeed; }
+        set { this.m_scrollSpeed = value; }
+    }
+
+    void Update()
+    {
+        // Time.deltaTime を使って滑らかに縦にスクロールさせる
+        this.transform.Translate(Vector2.down * Time.deltaTime * m_scrollSpeed);
+    }
+}
