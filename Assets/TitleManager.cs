c30fe34f89edfbaad61b3683f192da00dce68f83
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-07-29 13:55:41 +0900

    タイトル画面を作る

---
+++ Assets/TitleManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        Initiate.Fade(sceneName, Color.black, 2.5f);
+    }
+}
